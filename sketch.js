86f41260bd664fccba6bd436030743aa0321e033
Nabil Khan <nabilkhan@Nabils-MacBook-Pro.local>
2023-08-30 22:39:14 +0100

    padsfondas

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+
+function setup() 
+{
+    createCanvas(512,512);
+    fill(150,230,0);
+    stroke(255,0,255);
+
+}
+
+function draw()
+{
+    background(123,68,255);
+    
+    stroke(0,255,0,10)
+    fill(255,0,0,160);
+    // rect(100,100,200,200);
+
+    fill(255,0,0);
+    rect(100,100,100,100);
+
+    stroke(0);
+    fill(255,0,0);
+    ellipse(400,150,100,100);
+
+    fill(255,0,100);
+    strokeWeight(10)
+    stroke(255,0,100);
+    line(250,50,250,450);
+    strokeWeight(1);
+}
+
+
